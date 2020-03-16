d1b1faa6ca6155c8fb59f2ec4ed6f372f11442ef
iovw <billow.fun@gmail.com>
2020-03-16 21:20:49 +0800

    CI

--- src/run.ts
+++
@@ -1,8 +0,0 @@
-import Picgo from 'picgo'
-
-const app = new Picgo()
-
-
-import plug from './index'
-plug(app)
-app.upload(["https://raw.githubusercontent.com/ssloy/tinyrenderer/gh-pages/img/02-triangle/cfa0f3a9d9.png"])
