8df251cc1e39130aef829782ef2d28d79e23a369
iovw <billow.fun@gmail.com>
2020-03-16 17:04:34 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,47 +3,49 @@
  *   All rights reserved.
  */
 
-// const { readFile } = require("fs");
-// const probe = require("probe-image-size");
-// const sharp = require("sharp");
-// const { basename } = require("path");
-// const fetch = require("node-fetch");
+import { readFile } from "fs-extra";
+import probe from "probe-image-size";
+import sharp from "sharp";
+import { basename } from "path";
+import fetch from "node-fetch";
 
-// async function handle(ctx) {
-//   await Promise.all(
-//     ctx.input.map(async item => {
-//       try {
-//         let buffer = /https?:\/\//.test(item)
-//           ? await fetch(item).buffer
-//           : await readFile(item);
-//         buffer = await sharp(buffer)
-//           .webp({ lossless: true })
-//           .toBuffer();
-//         const { width, height } = probe.sync(buffer);
-//         ctx.output.push({
-//           buffer: webp_buffer,
-//           fileName: basename(item),
-//           width: width,
-//           height: height,
-//           extname: "webp"
-//         });
-//       } catch (e) {
-//         ctx.log.error(e);
-//       }
-//     })
-//   );
+import PicGo from 'picgo'
 
-//   return ctx;
-// }
+async function handle(ctx) {
+  await Promise.all(
+    ctx.input.map(async item => {
+      try {
+        let buffer = /https?:\/\//.test(item)
+          ? await (await fetch(item)).buffer()
+          : await readFile(item);
 
-export default function (ctx) {
-  const register = () => {
-    ctx.helper.transformer.register("webp", {
-      // handle
-    });
-  };
+        buffer = await sharp(buffer)
+          .webp({ lossless: true })
+          .toBuffer();
+        const { width, height } = probe.sync(buffer);
+        ctx.output.push({
+          buffer: buffer,
+          fileName: basename(item),
+          width: width,
+          height: height,
+          extname: "webp"
+        });
+      } catch (e) {
+        ctx.log.error(e);
+      }
+    })
+  );
+
+  return ctx;
+}
+
+export = function (ctx) {
   return {
-    register,
+    register: () => {
+      ctx.helper.transformer.register("webp", {
+        handle
+      });
+    },
     transformer: "webp"
   };
 };
---
+++ src/run.ts
@@ -0,0 +1,8 @@
+import Picgo from 'picgo'
+
+const app = new Picgo()
+
+
+import plug from './index'
+plug(app)
+app.upload(["C:/Users/i/Source/blog/assets/image/african-head-1024-true.png"])
