c91388c3550532e3f6681bc4cb757bcbaf4e1556
iovw <billow.fun@gmail.com>
2020-03-16 14:19:26 +0800

    &

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,49 @@
-import picgo from 'picgo'
+/*
+ *   Copyright (c) 2020 iovw
+ *   All rights reserved.
+ */
 
-export = (ctx: picgo) => {
+// const { readFile } = require("fs");
+// const probe = require("probe-image-size");
+// const sharp = require("sharp");
+// const { basename } = require("path");
+// const fetch = require("node-fetch");
+
+// async function handle(ctx) {
+//   await Promise.all(
+//     ctx.input.map(async item => {
+//       try {
+//         let buffer = /https?:\/\//.test(item)
+//           ? await fetch(item).buffer
+//           : await readFile(item);
+//         buffer = await sharp(buffer)
+//           .webp({ lossless: true })
+//           .toBuffer();
+//         const { width, height } = probe.sync(buffer);
+//         ctx.output.push({
+//           buffer: webp_buffer,
+//           fileName: basename(item),
+//           width: width,
+//           height: height,
+//           extname: "webp"
+//         });
+//       } catch (e) {
+//         ctx.log.error(e);
+//       }
+//     })
+//   );
+
+//   return ctx;
+// }
+
+export default function (ctx) {
   const register = () => {
-  }
+    ctx.helper.transformer.register("webp", {
+      // handle
+    });
+  };
   return {
-    register
-  }
-}
+    register,
+    transformer: "webp"
+  };
+};
